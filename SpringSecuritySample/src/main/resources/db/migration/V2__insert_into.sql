b029dc2b335c6a95aabc7ce4d9383b646518d77e
yasu <yasusi0117@live.jp>
2021-07-13 15:00:35 +0900

    テスト用DB用クリエート文インサート分追加

---
+++ SpringSecuritySample/src/main/resources/db/migration/V2__insert_into.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(username, password, name, rolename)
+VALUES('admin', '$2a$10$bCR1jXhdqbh1oC8ckXplxePYW5Kyb/VjN28MZx2PwXf1ybzLIFUQG', 'admin-name', 'ADMIN');
+INSERT INTO users(username, password, name, rolename)
+VALUES('user', '$2a$10$bCR1jXhdqbh1oC8ckXplxePYW5Kyb/VjN28MZx2PwXf1ybzLIFUQG', 'user-name', 'USER');
